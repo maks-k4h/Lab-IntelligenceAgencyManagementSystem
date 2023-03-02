52ebea261328d0556da75e0341f461a47b9cf5e0
Maksym Konevych <konevych.m.m@gmail.com>
2023-03-02 20:20:32 +0200

    feat(CoverRoles): implement basic functionality

---
+++ IntelligenceAgencyManagementSystem/Controllers/CoverRolesController.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using IntelligenceAgencyManagementSystem;
+
+namespace IntelligenceAgencyManagementSystem.Controllers
+{
+    public class CoverRolesController : Controller
+    {
+        private readonly IaDbContext _context;
+
+        public CoverRolesController(IaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CoverRoles
+        public async Task<IActionResult> Index()
+        {
+            var iaDbContext = _context.CoverRoles.Include(c => c.Gender);
+            return View(await iaDbContext.ToListAsync());
+        }
+
+        // GET: CoverRoles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.CoverRoles == null)
+            {
+                return NotFound();
+            }
+
+            var coverRole = await _context.CoverRoles
+                .Include(c => c.Gender)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (coverRole == null)
+            {
+                return NotFound();
+            }
+
+            return View(coverRole);
+        }
+
+        // GET: CoverRoles/Create
+        public IActionResult Create()
+        {
+            ViewData["GenderId"] = new SelectList(_context.Genders, "Id", "Name");
+            return View();
+        }
+
+        // POST: CoverRoles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,SecondName,GenderId,BirthDate,DeathDate,DateActivated,DateDeactivated,Legend,ActivitySummary")] CoverRole coverRole)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(coverRole);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GenderId"] = new SelectList(_context.Genders, "Id", "Name", coverRole.GenderId);
+            return View(coverRole);
+        }
+
+        // GET: CoverRoles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.CoverRoles == null)
+            {
+                return NotFound();
+            }
+
+            var coverRole = await _context.CoverRoles.FindAsync(id);
+            if (coverRole == null)
+            {
+                return NotFound();
+            }
+            ViewData["GenderId"] = new SelectList(_context.Genders, "Id", "Name", coverRole.GenderId);
+            return View(coverRole);
+        }
+
+        // POST: CoverRoles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,SecondName,GenderId,BirthDate,DeathDate,DateActivated,DateDeactivated,Legend,ActivitySummary")] CoverRole coverRole)
+        {
+            if (id != coverRole.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(coverRole);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CoverRoleExists(coverRole.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GenderId"] = new SelectList(_context.Genders, "Id", "Name", coverRole.GenderId);
+            return View(coverRole);
+        }
+
+        // GET: CoverRoles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.CoverRoles == null)
+            {
+                return NotFound();
+            }
+
+            var coverRole = await _context.CoverRoles
+                .Include(c => c.Gender)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (coverRole == null)
+            {
+                return NotFound();
+            }
+
+            return View(coverRole);
+        }
+
+        // POST: CoverRoles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.CoverRoles == null)
+            {
+                return Problem("Entity set 'IaDbContext.CoverRoles'  is null.");
+            }
+            var coverRole = await _context.CoverRoles.FindAsync(id);
+            if (coverRole != null)
+            {
+                _context.CoverRoles.Remove(coverRole);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CoverRoleExists(int id)
+        {
+          return (_context.CoverRoles?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
