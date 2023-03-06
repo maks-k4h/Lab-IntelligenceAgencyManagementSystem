c6ae55ddf3ab2c4350a267bba2658276533bce0d
Maksym Konevych <konevych.m.m@gmail.com>
2023-03-06 23:17:14 +0200

    feat(Models): add attributes

--- IntelligenceAgencyManagementSystem/Models/Task.cs
+++ IntelligenceAgencyManagementSystem/Models/Task.cs
@@ -4,22 +4,34 @@ using System.ComponentModel.DataAnnotations;
 
 namespace IntelligenceAgencyManagementSystem;
 
+[Display(Name = "Завдання")]
 public partial class Task
 {
+    [Display(Name = "Ідентифікатор")]
     public int Id { get; set; }
 
+    [Display(Name = "Ідентифікатр операції")]
+    [Required(ErrorMessage = "Необхідно вказати операцію")]
     public int OperationId { get; set; }
 
+    [Display(Name = "Назва")]
+    [Required(ErrorMessage = "Необхідно вказати назву")]
     public string Title { get; set; } = null!;
 
+    [Display(Name = "Опис")]
     public string? Description { get; set; }
 
+    [Display(Name = "Ідентифікатор статусу")]
+    [Required(ErrorMessage = "Необхідно вказати статус")]
     public int StatusId { get; set; }
 
+    [Display(Name = "Остання зміна")]
     public DateTime? DateStatusSet { get; set; }
 
+    [Display(Name = "Операція")]
     public virtual Operation? Operation { get; set; } = null!;
 
+    [Display(Name = "Статус")]
     public virtual TaskStatus? Status { get; set; } = null!;
 
     public virtual ICollection<TasksToWorkers> TasksToWorkers { get; } = new List<TasksToWorkers>();
--- IntelligenceAgencyManagementSystem/Models/TaskStatus.cs
+++ IntelligenceAgencyManagementSystem/Models/TaskStatus.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace IntelligenceAgencyManagementSystem;
 
+[Display(Name = "Статус завдання")]
 public partial class TaskStatus
 {
+    [Display(Name = "Ідентифікатор")]
     public int Id { get; set; }
 
+    [Display(Name = "Статус")]
+    [Required(ErrorMessage = "Необхідно вказати назву статусу")]
     public string Title { get; set; } = null!;
 
+    [Display(Name = "Подробиці статусу")]
     public string? Description { get; set; }
 
     public virtual ICollection<Task> Tasks { get; } = new List<Task>();
